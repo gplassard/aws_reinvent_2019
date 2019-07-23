1ee7791b7ccbe1118d1bc18ef968ec559d032bb8
gplassard <gplassard@mbpdegplassard.home>
2019-07-23 02:09:31 +0200

    Updates

---
+++ precache-manifest.625a9dac0ff05180fc6fd49ba92a89ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95af6d9f3d8506d4ce1be982c65e1f91",
+    "url": "/aws_reinvent_2019/index.html"
+  },
+  {
+    "revision": "9422f2587cc449f63dd8",
+    "url": "/aws_reinvent_2019/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/aws_reinvent_2019/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9422f2587cc449f63dd8",
+    "url": "/aws_reinvent_2019/static/js/main.9d672094.chunk.js"
+  },
+  {
+    "revision": "456e6ee8acd038f6bf21",
+    "url": "/aws_reinvent_2019/static/js/runtime~main.7e1749d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aws_reinvent_2019/static/media/logo.5d5d9eef.svg"
+  }
+]);
