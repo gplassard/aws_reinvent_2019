7e64d97bde2026b4314a3412107b5666e10c02b2
gplassard <gabriel.plassard@gmail.com>
2019-07-24 01:25:49 +0200

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import sessions from "./sessions.json";
+import { Session } from './model/index';
 
-ReactDOM.render(<App sessions={sessions}/>, document.getElementById('root'));
+ReactDOM.render(<App sessions={sessions as Session[]}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
