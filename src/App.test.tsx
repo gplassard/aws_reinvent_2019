f2ce2f8910d9b36d02445740729c1adb54382134
gplassard <gabriel.plassard@gmail.com>
2019-07-24 01:22:14 +0200

    fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import sessions from "./sessions.json";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App sessions={sessions}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
