4dd67b632f5cef85781b0c7e090d3ecef58eef2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-30 00:13:25 +0000

    Deploy gplassard/aws_reinvent_2019 to github.com/gplassard/aws_reinvent_2019.git:gh-pages

---
+++ precache-manifest.f127e20a7bd7536f1d12e761f2df02db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8736335dfa4fefd82709f04ea7cdc5e9",
+    "url": "/aws_reinvent_2019/index.html"
+  },
+  {
+    "revision": "c9161d4410197320bc03",
+    "url": "/aws_reinvent_2019/static/css/main.77a26f16.chunk.css"
+  },
+  {
+    "revision": "d9f0b82af1bd551328a4",
+    "url": "/aws_reinvent_2019/static/js/2.17faa4ef.chunk.js"
+  },
+  {
+    "revision": "c9161d4410197320bc03",
+    "url": "/aws_reinvent_2019/static/js/main.0f8e88b3.chunk.js"
+  },
+  {
+    "revision": "456e6ee8acd038f6bf21",
+    "url": "/aws_reinvent_2019/static/js/runtime~main.7e1749d5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aws_reinvent_2019/precache-manifest.62879a7aa9d636a64e55f155e665b107.js"
+  "/aws_reinvent_2019/precache-manifest.f127e20a7bd7536f1d12e761f2df02db.js"
 );
 
 self.addEventListener('message', (event) => {
