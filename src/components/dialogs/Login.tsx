7807bf41afe744b3ffc903827849ea33af43cac3
Gabriel Plassard <gabriel.plassard@gmail.com>
2019-09-17 21:14:56 +0200

    login style & errors

--- src/components/dialogs/Login.tsx
+++ src/components/dialogs/Login.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, Tabs, Tab, TextField, Button, FormControl } from "@material-ui/core";
+import { Dialog, DialogContent, Tabs, Tab, TextField, Button, FormControl, Box } from "@material-ui/core";
 import React from 'react';
 import TabPanel from "../utils/TabPanel";
 import "./Login.scss";
@@ -6,7 +6,7 @@ import { auth, googleProvider } from "../../services/FirebaseService";
 
 interface Props {
     open: boolean
-    onClose: (auth: firebase.auth.UserCredential) => any
+    onClose: (auth?: firebase.auth.UserCredential) => any
 }
 
 
@@ -14,33 +14,55 @@ const Login: React.FC<Props> = (props: Props) => {
     const [tabIndex, setTabIndex] = React.useState(0);
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [error, setError] = React.useState<any | null>(null);
 
     function panelChange(event: any, newValue: number) {
+        setError(null);
         setTabIndex(newValue);
     }
      
     async function loginAnonymous() {
-        const cred = await auth.signInAnonymously();
-        props.onClose(cred);
+        try {
+            const cred = await auth.signInAnonymously();
+            props.onClose(cred);
+        } catch (error) {
+            setError(error);
+        }
     }
 
     async function loginGoogle() {
-        const cred = await auth.signInWithPopup(googleProvider);
-        props.onClose(cred);
+        try {
+            const cred = await auth.signInWithPopup(googleProvider);
+            props.onClose(cred);
+        } catch (error) {
+            setError(error);
+        }
     }
 
     async function loginEmail() {
-        const cred = await auth.signInWithEmailAndPassword(email, password);
-        props.onClose(cred);
+        try {
+            const cred = await auth.signInWithEmailAndPassword(email, password);
+            props.onClose(cred);
+        } catch (error) {
+            setError(error);
+        }
     }
 
     async function createAccount() {
-        const cred = await auth.createUserWithEmailAndPassword(email, password);
-        props.onClose(cred);
+        try {
+            const cred = await auth.createUserWithEmailAndPassword(email, password);
+            props.onClose(cred);
+        } catch (error) {
+            setError(error);
+        }
+    }
+
+    async function handleClose() {
+        props.onClose();
     }
 
     return (
-        <Dialog open={props.open || true} fullWidth>
+        <Dialog open={props.open} onClose={handleClose} fullWidth>
             <DialogContent className="login-dialog">
                 <Tabs value={tabIndex} onChange={panelChange}>
                     <Tab label="Login"/>
@@ -51,6 +73,7 @@ const Login: React.FC<Props> = (props: Props) => {
                         <TextField id="email" label="Email" required autoFocus fullWidth onChange={e => setEmail(e.target.value)}></TextField>
                         <TextField id="password" label="Password" required autoFocus fullWidth type="password" onChange={e => setPassword(e.target.value)}></TextField>
                         {/* <Typography><Box color="text.hint" textAlign="right">Forgotten password</Box></Typography> */}
+                        { error && error.message ? <Box color="error.main">{error.message}</Box> : null}
                         <Button variant="contained" color="primary" onClick={loginEmail}>Submit</Button>
                         <div className="alternative-login">
                             <Button onClick={loginAnonymous}>Anonymous</Button>
@@ -62,6 +85,7 @@ const Login: React.FC<Props> = (props: Props) => {
                     <FormControl fullWidth className="login-form" onSubmit={createAccount}>
                         <TextField id="email" label="Email" required autoFocus fullWidth onChange={e => setEmail(e.target.value)}></TextField>
                         <TextField id="password" label="Password" required autoFocus fullWidth type="password" onChange={e => setPassword(e.target.value)}></TextField>
+                        { error && error.message ? <Box color="error.main">{error.message}</Box> : null}
                         <Button variant="contained" color="primary" onClick={createAccount}>Create account</Button>
                     </FormControl>
             </TabPanel>
